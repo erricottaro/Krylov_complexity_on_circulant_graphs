84fd28bd044218ad7d46ddef6e5cfd8f4730d4bb
Erricottaro <lucaferretti.ipad@gmail.com>
2026-03-09 11:56:04 +0100

    I don't know what's going on

--- src/main.cpp
+++ src/main.cpp
@@ -9,21 +9,22 @@ int main(){
     //cout << I << endl;
 
     double theta_max = M_PI;
-    int n_theta = 1000;
+    int n_theta = 10;
 
     double phi_max = 2*M_PI;
-    int n_phi = 1000;
+    int n_phi = 10;
 
     double delta_theta = theta_max/n_theta;
     double delta_phi = phi_max/n_phi;
 
     for(int i=0; i<n_theta; i++){
         double theta = i*delta_theta;
+        cout << i << endl;
         for(int j=0; i<n_phi; j++){
             double phi = j*delta_phi;
-            double complexity = c_bar(theta, phi);
+            cout << j << endl;
+            //double complexity = c_bar(theta, phi);
         }
-        //cout << theta << endl;
         //double c = c_bar(theta, M_PI/2.);
     }
 
