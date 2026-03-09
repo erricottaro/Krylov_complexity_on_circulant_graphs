13bbb4eea648256308ff78ff2c0d4994adada3aa
Erricottaro <lucaferretti.ipad@gmail.com>
2026-03-09 11:44:53 +0100

    ready to run c++ code

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "triangle.h"
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -8,14 +9,22 @@ int main(){
     //cout << I << endl;
 
     double theta_max = M_PI;
-    int n_theta = 10;
+    int n_theta = 1000;
+
+    double phi_max = 2*M_PI;
+    int n_phi = 1000;
 
     double delta_theta = theta_max/n_theta;
+    double delta_phi = phi_max/n_phi;
 
     for(int i=0; i<n_theta; i++){
         double theta = i*delta_theta;
+        for(int j=0; i<n_phi; j++){
+            double phi = j*delta_phi;
+            double complexity = c_bar(theta, phi);
+        }
         //cout << theta << endl;
-        //cout << c_0(theta, M_PI/2.) << endl;
+        //double c = c_bar(theta, M_PI/2.);
     }
 
     return 0;
--- src/triangle.cpp
+++ src/triangle.cpp
@@ -15,3 +15,48 @@ complex<double> c_0(double theta1, double phi1, double theta2, double phi2){
 
     return norm*(c1 + phase1*s1*c2 + phase2*s1*s2);
 }
+
+double c_plus(double theta1, double phi1, double theta2, double phi2){
+    double norm = sqrt(2./3.);
+
+    double c1 = cos(theta1/2.);
+    double s1 = sin(theta1/2.); 
+    double c2 = cos(theta2/2.);
+    double s2 = sin(theta2/2.);
+
+    double shift = 2*M_PI/3.;
+
+    double c1_shifted = cos(phi1+shift);
+    double c1_shifted2 = cos(phi1+2*shift);
+    double c2_shifted = cos(phi2+shift);
+    double c2_shifted2 = cos(phi2+2*shift);
+    double cdiff_shifted = cos(phi1-phi2 + shift);
+    double cdiff_shifted2 = cos(phi1-phi2 + 2*shift);
+
+    double body = 1 + c1*s1*c2*(c1_shifted+c1_shifted2) + c1*s1*s2*(c2_shifted+c2_shifted2) + (s1*s1)*s2*c2*(cdiff_shifted+cdiff_shifted2);
+
+    return norm*sqrt(body);
+}
+
+double a_0(double theta1, double phi1, double theta2, double phi2){
+    double first = norm(c_0(theta1, phi1, theta2, phi2));
+    double second = norm(c_plus(theta1, phi1, theta2, phi2));
+
+    return 2.*first - second;
+}
+
+double b_1(double theta1, double phi1, double theta2, double phi2){
+    double first = norm(c_0(theta1, phi1, theta2, phi2))*pow((2.-a_0(theta1, phi1, theta2, phi2)), 2);
+    double second = norm(c_plus(theta1, phi1, theta2, phi2))*pow((1.+a_0(theta1, phi1, theta2, phi2)), 2);
+
+    return sqrt(first+second);
+}
+
+double c_bar(double theta1, double phi1, double theta2, double phi2){
+    double den = 1./pow(b_1(theta1, phi1, theta2, phi2), 2);
+    double first = pow(norm(c_0(theta1, phi1, theta2, phi2))*(2.-a_0(theta1, phi1, theta2, phi2)), 2);
+    double second = pow(norm(c_plus(theta1, phi1, theta2, phi2))*(1.+a_0(theta1, phi1, theta2, phi2)), 2);
+
+    return den*(first + second);
+}
+
