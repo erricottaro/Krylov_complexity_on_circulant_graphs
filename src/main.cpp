9657ac47649591d5059564eca3a7bcfbc52d8b68
Erricottaro <lucaferretti.ipad@gmail.com>
2026-03-09 13:47:27 +0100

    generated output (10 GBgit add src)

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,10 @@ int main(){
     //cout << I << endl;
 
     double theta_max = M_PI;
-    int n_theta = 1000;
+    int n_theta = 100;
 
     double phi_max = 2*M_PI;
-    int n_phi = 1000;
+    int n_phi = 100;
 
     double delta_theta = theta_max/n_theta;
     double delta_phi = phi_max/n_phi;
@@ -22,8 +22,9 @@ int main(){
 
     output.open("../output/output.out");
 
-    output << setw(20) << "theta1" << setw(20) << "phi1" << setw(20) << "theta2" << setw(20) << "phi2" << endl;
+    output << setw(20) << "theta1" << setw(20) << "phi1" << setw(20) << "theta2" << setw(20) << "phi2" << setw(20) << "C_bar" << endl;
 
+    /*
     for(int i=0; i<n_theta; i++){
         double theta1 = i*delta_theta;
         for(int j=0; j<n_phi; j++){
@@ -32,7 +33,8 @@ int main(){
             output << setw(20) << theta1 << setw(20) << phi1 << setw(20) << 0. << setw(20) << 0. << endl;
         }
     }
-    /*
+    */
+    
     for(int i=0; i<n_theta; i++){
         double theta1 = i*delta_theta;
         for(int j=0; j<n_phi; j++){
@@ -41,13 +43,13 @@ int main(){
                 double theta2 = k*delta_theta;
                 for(int l=0; l<n_phi; l++){
                     double phi2 = l*delta_phi;
-                    c_bar(theta1, phi1, theta2, phi2);
-                    output << setw(20) << theta1 << setw(20) << phi1 << setw(20) << theta2 << setw(20) << phi2 << endl;
+                    double c_b = c_bar(theta1, phi1, theta2, phi2);
+                    output << setw(20) << theta1 << setw(20) << phi1 << setw(20) << theta2 << setw(20) << phi2 << setw(20) << c_b << endl;
                 }
             }
         }
     }
-    */
+    
 
     output.close();
 
