4c64bb5dbfdcd7bc623f57b9101f4f1bc08c49bb
Erricottaro <lucaferretti.ipad@gmail.com>
2026-03-23 16:56:40 +0100

    qubit from cartesian coordinates

--- states_gen.py
+++ states_gen.py
@@ -8,17 +8,16 @@ def qubit(theta, phi):
     state = np.array([c,s*phase])
     return state
 
-#returns a qubit given a 3D point on a sphere
+#returns a qubit given cartesian coordinates on a CP1
 def qubit_XYZ(x):
     theta = np.ndarray(x.shape[0])
     phi = np.ndarray(x.shape[0])
 
-    x1 = x[:,1]
-    x2 = x[:,2]
-    x3 = x[:,3]
+    z0 = x[:,0]
+    z1 = x[:,1]
 
-    theta = np.acos(x3)
-    phi = np.atan2(x2, x1)
+    theta = 2*np.arccos(z0)
+    phi = np.angle(z1)
 
     state = qubit(theta, phi)
 
@@ -27,11 +26,11 @@ def qubit_XYZ(x):
 #Returns a qutrit according to Haar parametrization
 def qutrit(thetas, phis):
     #rotation angles
-    th1 = thetas[0]
-    th2 = thetas[1]
+    th1 = thetas[:,0]
+    th2 = thetas[:,1]
     #complex phases
-    phi1 = phis[0]
-    phi2 = phis[1]
+    phi1 = phis[:,0]
+    phi2 = phis[:,1]
 
     #cosines and sines
     c1 = np.cos(th1/2.)
@@ -53,3 +52,11 @@ def qutrit(thetas, phis):
     state = np.array([coeff1, coeff2, coeff3])
 
     return state
+
+#returns a qutrit given cartesian coordinates on a S5/S1
+def qutrit_XYZ(x):
+    theta1 = np.ndarray(x.shape[0])
+    theta2 = np.ndarray(x.shape[0])
+    phi1 = np.ndarray(x.shape[0])
+    phi2 = np.ndarray(x.shape[0])
+
